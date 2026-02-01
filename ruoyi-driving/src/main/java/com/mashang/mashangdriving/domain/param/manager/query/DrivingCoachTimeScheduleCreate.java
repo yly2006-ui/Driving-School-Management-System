67e42654361ff63a87719f31a1be46905ec65e7a
YL <3154563562@qq.com>
2026-02-01 17:36:28 +0800

    优化

--- ruoyi-driving/src/main/java/com/mashang/mashangdriving/domain/param/manager/query/DrivingCoachTimeScheduleCreate.java
+++ ruoyi-driving/src/main/java/com/mashang/mashangdriving/domain/param/manager/query/DrivingCoachTimeScheduleCreate.java
@@ -1,6 +1,7 @@
 package com.mashang.mashangdriving.domain.param.manager.query;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -11,9 +12,11 @@ public class DrivingCoachTimeScheduleCreate {
 
     @NotNull(message = "开始时间不能为空") // 核心：校验startTime不能为null
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ApiModelProperty(value = "开始时间",required = true)
     private LocalDateTime startTime;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @NotNull(message = "开始时间不能为空") // 核心：校验startTime不能为null
+    @ApiModelProperty(value = "结束时间",required = true)
     private LocalDateTime endTime;
 //    private String status;
 
