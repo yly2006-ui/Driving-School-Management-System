5d75cea3fb48ff367632b3e1006e2fa4103b0733
YL <3154563562@qq.com>
2026-02-08 16:45:58 +0800

    优化

--- ruoyi-driving/src/main/java/com/mashang/mashangdriving/controller/manager/DrivingCoachTimeScheduleController.java
+++ ruoyi-driving/src/main/java/com/mashang/mashangdriving/controller/manager/DrivingCoachTimeScheduleController.java
@@ -154,7 +154,7 @@ public class DrivingCoachTimeScheduleController extends BaseController {
 
     @DeleteMapping("/delete")
     @ApiOperation("批量取消可预约时间安排")
-    public R deleteById(@RequestBody List<DrivingCoachTimeScheduleDelete> deleteList) {
+    public R deleteById( @RequestBody @Validated List<DrivingCoachTimeScheduleDelete> deleteList) {
         if (deleteList == null || deleteList.isEmpty()) {
             return R.fail("待取消的时间安排列表不能为空");
         }
