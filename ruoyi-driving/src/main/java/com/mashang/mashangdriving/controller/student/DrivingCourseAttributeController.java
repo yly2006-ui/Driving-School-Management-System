75ca1b46c4047861621da00a812abdfc4f6470de
YL <3154563562@qq.com>
2026-02-11 14:19:31 +0800

    优化

--- ruoyi-driving/src/main/java/com/mashang/mashangdriving/controller/student/DrivingCourseAttributeController.java
+++ ruoyi-driving/src/main/java/com/mashang/mashangdriving/controller/student/DrivingCourseAttributeController.java
@@ -45,28 +45,33 @@ public class DrivingCourseAttributeController extends BaseController {
         }
     }
 
-    @ApiOperation("分页查询学习资料")
+    @ApiOperation("查询学习资料列表")
     @GetMapping("/list")
-    public TableDataInfo<List<DrivingCourseStudentListVo>> list(@Validated PageQuery pageQuery, String name){
-        Page<DrivingCourseAttribute> page =new Page<>(pageQuery.getPageNum(),pageQuery.getPageSize());
+    public TableDataInfo<List<DrivingCourseStudentListVo>> list( String name){
         LambdaQueryWrapper<DrivingCourseAttribute>lambdaQueryWrapper=new LambdaQueryWrapper<>();
         lambdaQueryWrapper.like(StringUtils.isNotEmpty(name),DrivingCourseAttribute::getAttributeName,name);
-        Page<DrivingCourseAttribute> attributePage = drivingCourseAttributeService.page(page, lambdaQueryWrapper);
-
-        List<DrivingCourseAttribute> records = attributePage.getRecords();
-        List<DrivingCourseStudentListVo> drivingCourseAttributeVOS=new ArrayList<>();
-            for (DrivingCourseAttribute record : records) {
-                DrivingCourseStudentListVo drivingCourseStudentListVo=new DrivingCourseStudentListVo();
-                Integer attributeId = Math.toIntExact(record.getAttributeId());
-                String attributeName = record.getAttributeName();
-                String introduction = record.getIntroduction();
-                drivingCourseStudentListVo.setAttributeId(attributeId);
-                drivingCourseStudentListVo.setAttributeName(attributeName);
-                drivingCourseStudentListVo.setIntroduction(introduction);
-                drivingCourseAttributeVOS.add(drivingCourseStudentListVo);
-
+        List<DrivingCourseAttribute> list = drivingCourseAttributeService.list(lambdaQueryWrapper);
+        List<DrivingCourseStudentListVo> vos = DrivingCourseAttributeMapping.INSTANCE.toList(list);
+        for (DrivingCourseStudentListVo vo : vos) {
+            Integer attributeId = vo.getAttributeId();
+            List<DrivingCourseAttributeVO> drivingCourseAttributeVOS = drivingCourseAttributeService.
+                    selectByCourseId(Long.valueOf(attributeId), SecurityUtils.getUserId());
+            for (DrivingCourseAttributeVO drivingCourseAttributeVO : drivingCourseAttributeVOS) {
+                String totalTime = drivingCourseAttributeVO.getTotalTime();
+                String courseCount = drivingCourseAttributeVO.getCourseCount();
+                String studyPersonTotal = drivingCourseAttributeVO.getStudyPersonTotal();
+                String percentage = drivingCourseAttributeVO.getPercentage();
+                vo.setTotalTime(totalTime);
+                vo.setCourseCount(courseCount);
+                vo.setStudyPersonTotal(studyPersonTotal);
+                vo.setPercentage(percentage);
+                vos.add(vo);
+            }
         }
-        return getDataTable(drivingCourseAttributeVOS,attributePage.getTotal());
+
+
+
+        return getDataTable(vos);
     }
 
     @ApiOperation("新增学习课程")
--- ruoyi-driving/src/main/java/com/mashang/mashangdriving/mapping/student/DrivingCourseAttributeMapping.java
+++ ruoyi-driving/src/main/java/com/mashang/mashangdriving/mapping/student/DrivingCourseAttributeMapping.java
@@ -3,11 +3,17 @@ package com.mashang.mashangdriving.mapping.student;
 import com.mashang.mashangdriving.domain.entity.DrivingCourseAttribute;
 import com.mashang.mashangdriving.domain.param.student.create.DrivingCourseAttributeCreate;
 import com.mashang.mashangdriving.domain.param.student.update.DrivingCourseAttributeUpdate;
+import com.mashang.mashangdriving.domain.vo.student.DrivingCourseStudentListVo;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
 @Mapper
 public interface DrivingCourseAttributeMapping {
     DrivingCourseAttributeMapping INSTANCE = Mappers.getMapper(DrivingCourseAttributeMapping.class);
+
+    List<DrivingCourseStudentListVo> toList(List<DrivingCourseAttribute> drivingCourseAttributes);
 
     DrivingCourseAttribute toCreate(DrivingCourseAttributeCreate drivingCourseAttributeCreate);
 
